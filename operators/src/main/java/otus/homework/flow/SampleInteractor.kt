d0ad4cb98baa36635cbd1c7bb66d6b179bc0cbfc
Vladimir Chertilov <chertilov@reksoft.ru>
2021-05-07 15:13:29 +0300

    flow/SampleInteractor tasks implemented

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -5,21 +5,25 @@ import kotlinx.coroutines.flow.*
 
 @ExperimentalCoroutinesApi
 class SampleInteractor(
-    private val sampleRepository: SampleRepository
+        private val sampleRepository: SampleRepository
 ) {
 
     /**
      * Реализуйте функцию task1 которая последовательно:
      * 1) возводит числа в 5ую степень
-     * 2) убирает чила <= 20
+     * 2) убирает числа <= 20
      * 3) убирает четные числа
      * 4) добавляет постфикс "won"
      * 5) берет 3 первых числа
      * 6) возвращает результат
      */
-    fun task1(): Flow<String> {
-        return flowOf()
-    }
+    fun task1(): Flow<String> = sampleRepository.produceNumbers()
+            .map { it * 5 }
+//            .map { it.toDouble().pow(5.0) }
+            .filter { it > 20 }
+            .filter { it % 2 != 0 }
+            .map { "$it won" }
+            .take(3)
 
     /**
      * Классическая задача FizzBuzz с небольшим изменением.
@@ -28,26 +32,34 @@ class SampleInteractor(
      * Если входное число делится на 15 - эмитим само число и после него эмитим строку FizzBuzz
      * Если число не делится на 3,5,15 - эмитим само число
      */
-    fun task2(): Flow<String> {
-        return flowOf()
-    }
+    fun task2(): Flow<String> = sampleRepository.produceNumbers()
+            .transform {
+                emit("$it")
+                when {
+                    it % 15 == 0 -> emit("FizzBuzz")
+                    it % 3 == 0 -> emit("Fizz")
+                    it % 5 == 0 -> emit("Buzz")
+                }
+            }
 
     /**
      * Реализуйте функцию task3, которая объединяет эмиты из двух flow и возвращает кортеж Pair<String,String>(f1,f2),
      * где f1 айтем из первого флоу, f2 айтем из второго флоу.
-     * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
+     * Если айтемы в одном из флоу кончились, то результирующий флоу также должен закончиться
      */
-    fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
-    }
+    fun task3(): Flow<Pair<String, String>> = sampleRepository.produceColors()
+            .zip(sampleRepository.produceForms()) { color, form -> Pair(color, form) }
 
     /**
-     * Реализайте функцию task4, которая обрабатывает IllegalArgumentException и в качестве фоллбека
+     * Реализуйте функцию task4, которая обрабатывает IllegalArgumentException и в качестве фоллбека
      * эмитит число -1.
      * Если тип эксепшена != IllegalArgumentException, пробросьте его дальше
-     * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
+     * При любом исходе, будь то выброс исключения или успешная отработка функции, вызовите метод dotsRepository.completed()
      */
-    fun task4(): Flow<Int> {
-        return flowOf()
-    }
+    fun task4(): Flow<Int> = sampleRepository.produceNumbers()
+            .catch {
+                if (it is IllegalArgumentException) emit(-1)
+                else throw it
+            }
+            .onCompletion { sampleRepository.completed() }
 }
