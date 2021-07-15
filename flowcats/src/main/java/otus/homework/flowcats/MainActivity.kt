0df6a1a3103265c763cbd69bfa9ce564f85c225a
abzal <abz@mail.com>
2021-07-15 10:46:40 +0600

    part 1 - migrate to StateFlows

--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -2,7 +2,10 @@ package otus.homework.flowcats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +17,18 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
+        lifecycleScope.launchWhenStarted {
+            catsViewModel.catsFlow.collect{
+                when(it) {
+                    Empty -> {}
+                    is Error -> {
+                        Toast.makeText(this@MainActivity, it.t.toString(), Toast.LENGTH_LONG).show()
+                    }
+                    is Success<*> -> {
+                        view.populate(it.data as Fact)
+                    }
+                }
+            }
         }
     }
 }
