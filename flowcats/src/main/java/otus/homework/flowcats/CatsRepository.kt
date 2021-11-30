c1df6b612e72a4e8fd6f5658d5f17582b0ec4b9c
Baxtiyor <baxtiyor@nurmatov.uz>
2021-11-30 17:10:45 +0500

    Requested changes done: 1. https://github.com/Otus-Android/FlowHomework/pull/39#discussion_r757768594

--- flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
@@ -1,11 +1,13 @@
 package otus.homework.flowcats
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class CatsRepository(
     private val catsService: CatsService,
-    private val refreshIntervalMs: Long = 5000
+    private val refreshIntervalMs: Long = 5000,
 ) {
 
     fun listenForCatFacts() = flow {
@@ -14,5 +16,5 @@ class CatsRepository(
             emit(latestNews)
             delay(refreshIntervalMs)
         }
-    }
+    }.flowOn(Dispatchers.IO)
 }
--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -3,13 +3,11 @@ package otus.homework.flowcats
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class CatsViewModel(
     private val catsRepository: CatsRepository,
@@ -19,12 +17,10 @@ class CatsViewModel(
 
     init {
         viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().catch { throwable ->
-                    _catsStateFlow.emit(Result.Error(throwable.localizedMessage ?: "Unknown"))
-                }.collect { fact ->
-                    _catsStateFlow.emit(Result.Success(fact))
-                }
+            catsRepository.listenForCatFacts().catch { throwable ->
+                _catsStateFlow.emit(Result.Error(throwable.localizedMessage ?: "Unknown"))
+            }.collect { fact ->
+                _catsStateFlow.emit(Result.Success(fact))
             }
         }
     }
