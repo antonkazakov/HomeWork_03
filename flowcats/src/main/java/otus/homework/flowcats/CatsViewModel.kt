2e120b34881bb1b21d6a099c3c9942945f530db9
Necris174 <plotnicovvyu@gmail.com>
2022-01-27 09:34:33 +0500

    LiveData to Flow

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -2,9 +2,13 @@ package otus.homework.flowcats
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+
 
 class CatsViewModel(
     private val catsRepository: CatsRepository
@@ -13,19 +17,37 @@ class CatsViewModel(
     private val _catsLiveData = MutableLiveData<Fact>()
     val catsLiveData: LiveData<Fact> = _catsLiveData
 
+    private val _catsFlow = MutableStateFlow<Result<Fact>>(Result.Empty())
+    val catsFlow: StateFlow<Result<Fact>> = _catsFlow
+
+
+
+
+
+
     init {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+                catsRepository.listenForCatFacts()
+                    .catch { _catsFlow.value = Result.Error(it.message) }
+                    .collect {
+                    _catsFlow.value = it
                 }
             }
         }
     }
 }
 
+
 class CatsViewModelFactory(private val catsRepository: CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T =
         CatsViewModel(catsRepository) as T
+}
+
+
+sealed class Result<T> {
+    class Success<T>(val data: T) : Result<T>()
+    class Error<T>(val message: String?) : Result<T>()
+    class Empty<T>: Result<T>()
 }
