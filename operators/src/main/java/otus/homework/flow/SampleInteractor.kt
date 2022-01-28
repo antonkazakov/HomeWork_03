d44f127a70a074d16f2c0bd4a58bb72b341166bf
Ruslan <r.husainov@alpinadigital.ru>
2022-01-28 12:40:47 +0300

    Реализация функции с использование flow операторов

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -18,7 +18,11 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .map { it * 5 }
+            .filter { it >= 20 && ( it % 2  != 0 )}
+            .map { "$it won" }
+            .take(3)
     }
 
     /**
@@ -29,7 +33,17 @@ class SampleInteractor(
      * Если число не делится на 3,5,15 - эмитим само число
      */
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .transform {
+                emit("$it")
+                when{
+                    it % 15 == 0 -> emit("FizzBuzz")
+                    it % 3 == 0 -> emit("Fizz")
+                    it % 5 == 0 -> emit("Buzz")
+                    else -> Unit
+                }
+            }
+
     }
 
     /**
@@ -38,7 +52,8 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        return sampleRepository.produceColors()
+            .zip(sampleRepository.produceForms()) { one, two -> Pair(one, two) }
     }
 
     /**
@@ -48,6 +63,13 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch {
+                sampleRepository.completed()
+                when (it) {
+                    is IllegalArgumentException -> emit(-1)
+                    else -> throw it
+                }
+            }
     }
 }
