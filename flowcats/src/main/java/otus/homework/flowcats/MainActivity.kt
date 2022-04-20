bf30a92b727127357aa56e54bb03e2c0f03db809
Alexsei Zhadov <exfaust@yandex.ru>
2022-04-20 13:39:26 +0300

    FlowHomework

--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -2,7 +2,15 @@ package otus.homework.flowcats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +22,11 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
-        }
+        catsViewModel.catsStateFlow.onEach { result ->
+            when (result) {
+                is Result.Success<*> -> (result.data as? Fact)?.let { view.populate(it) }
+                is Result.Error -> {}
+            }
+        }.launchIn(lifecycleScope)
     }
 }
--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -2,6 +2,7 @@ package otus.homework.flow
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
+import kotlin.math.pow
 
 @ExperimentalCoroutinesApi
 class SampleInteractor(
@@ -17,9 +18,13 @@ class SampleInteractor(
      * 5) берет 3 первых числа
      * 6) возвращает результат
      */
-    fun task1(): Flow<String> {
-        return flowOf()
-    }
+    fun task1(): Flow<String> =
+        sampleRepository.produceNumbers()
+            .map { it.toFloat().pow(5).toInt() } // тест ожидает умножения на 5
+            .filter { it > 20 }
+            .filter { it % 2 != 0 }
+            .map { "$it won" }
+            .take(3)
 
     /**
      * Классическая задача FizzBuzz с небольшим изменением.
@@ -28,18 +33,26 @@ class SampleInteractor(
      * Если входное число делится на 15 - эмитим само число и после него эмитим строку FizzBuzz
      * Если число не делится на 3,5,15 - эмитим само число
      */
-    fun task2(): Flow<String> {
-        return flowOf()
-    }
+    fun task2(): Flow<String> =
+        sampleRepository.produceNumbers()
+            .transform {
+                emit(it.toString())
+                when{
+                    it % 15 == 0 -> { emit("FizzBuzz") }
+                    it % 3 == 0 -> { emit("Fizz") }
+                    it % 5 == 0 -> { emit("Buzz") }
+                }
+            }
 
     /**
      * Реализуйте функцию task3, которая объединяет эмиты из двух flow и возвращает кортеж Pair<String,String>(f1,f2),
      * где f1 айтем из первого флоу, f2 айтем из второго флоу.
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
-    fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
-    }
+    fun task3(): Flow<Pair<String, String>> =
+        sampleRepository.produceColors().zip(sampleRepository.produceForms()) { f1, f2 ->
+            f1 to f2
+        }
 
     /**
      * Реализайте функцию task4, которая обрабатывает IllegalArgumentException и в качестве фоллбека
@@ -47,7 +60,12 @@ class SampleInteractor(
      * Если тип эксепшена != IllegalArgumentException, пробросьте его дальше
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
-    fun task4(): Flow<Int> {
-        return flowOf()
-    }
+    fun task4(): Flow<Int> =
+        sampleRepository.produceNumbers()
+            .catch {
+                sampleRepository.completed()
+                if (it is IllegalArgumentException)
+                    emit(-1)
+                else throw it
+            }
 }
