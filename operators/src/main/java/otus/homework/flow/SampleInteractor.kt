024b709fac9d071d62c5aa5177eed94485ec8731
Roman Belyanin <bel.r.s@yandex.ru>
2022-05-01 15:49:29 +0300

    3. В случае если возникнет ошибка в стриме, нужно заэмитить айтем Result.Error* Реализовать функции с использование flow операторов task1-task4

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -18,7 +18,12 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .map { it * 5 }
+            .filter { it > 20 }
+            .filter { (it % 2) != 0 }
+            .map { "$it won" }
+            .take(3)
     }
 
     /**
@@ -29,7 +34,27 @@ class SampleInteractor(
      * Если число не делится на 3,5,15 - эмитим само число
      */
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .transform {
+                when {
+                    it % 15 == 0 -> {
+                        emit(it.toString())
+                        emit("FizzBuzz")
+                    }
+                    it % 3 == 0 -> {
+                        emit(it.toString())
+                        emit("Fizz")
+                    }
+                    it % 5 == 0 -> {
+                        emit(it.toString())
+                        emit("Buzz")
+                    }
+
+                    else -> {
+                        emit(it.toString())
+                    }
+                }
+            }
     }
 
     /**
@@ -38,7 +63,13 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        val color = sampleRepository.produceColors()
+        val form = sampleRepository.produceForms()
+
+        val pair = color.zip(form) { f1, f2 ->
+            Pair(f1, f2)
+        }
+        return pair
     }
 
     /**
@@ -48,6 +79,17 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch { e ->
+                if (e is IllegalArgumentException) {
+                    emit(-1)
+                } else {
+                    throw e
+                }
+            }
+            .onCompletion {
+                sampleRepository.completed()
+            }
+
     }
 }
