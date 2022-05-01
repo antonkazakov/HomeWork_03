b3e36ddd0179a073de42e34a2d9c6805deec424e
Elisey Lobanov <crimsonorama@gmail.com>
2022-05-01 19:08:14 +0300

    Приложение переведено на StateFlow

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -2,22 +2,29 @@ package otus.homework.flowcats
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.lang.Error
 
 class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsFlow = MutableStateFlow<Result<Fact>>(Result.Success(Fact("", false, "", "", "", false, "", "", "")))
+    val catsFlow : StateFlow<Result<Fact>> = _catsFlow
 
     init {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
                 catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+                    try {
+                        _catsFlow.value = Result.Success(it)
+                    } catch (e: Error) {
+                        _catsFlow.value = Result.Error(e.message, e)
+                    }
                 }
             }
         }
