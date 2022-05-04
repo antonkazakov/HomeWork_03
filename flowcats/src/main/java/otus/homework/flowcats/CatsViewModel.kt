ce8f324487319895263a501130bb313890820398
kamzolov-nik <kamzolov-nik@yandex-team.ru>
2022-05-04 19:04:52 +0300

    LiveData to StateFlow

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,22 +1,24 @@
 package otus.homework.flowcats
 
-import androidx.lifecycle.*
-import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsFlow = MutableStateFlow<Fact?>(null)
+    val catsFlow: StateFlow<Fact?> = _catsFlow
 
     init {
         viewModelScope.launch {
                 catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+                    _catsFlow.value = it
             }
         }
     }
