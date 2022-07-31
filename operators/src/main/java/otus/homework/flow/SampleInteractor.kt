a2a6fa416cfe837341d2d3c0276b6c393980025a
ildarserazitdinov <serazitdinov.ildar@gmail.com>
2022-07-31 18:30:20 +0300

    task4

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -77,6 +77,14 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch { e ->
+                when (e) {
+                    is IllegalArgumentException -> emit(-1)
+                    else -> throw e
+                }
+            }.onCompletion {
+                sampleRepository.completed()
+            }
     }
 }
