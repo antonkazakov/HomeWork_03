eaaba75d94815455ca7feda560e025251ea0f617
yolis_storn <elisstormbreaker@gmail.com>
2022-08-30 19:50:31 +0300

    Выполнены задачи в SampleInteractor

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -17,9 +17,14 @@ class SampleInteractor(
      * 5) берет 3 первых числа
      * 6) возвращает результат
      */
-    fun task1(): Flow<String> {
-        return flowOf()
-    }
+    fun task1(): Flow<String> =
+        sampleRepository
+            .produceNumbers()
+            .map { it.times(5) }
+            .filter { it > 20 }
+            .filter { it % 2 != 0 }
+            .map { "$it won" }
+            .take(3)
 
     /**
      * Классическая задача FizzBuzz с небольшим изменением.
@@ -28,18 +33,33 @@ class SampleInteractor(
      * Если входное число делится на 15 - эмитим само число и после него эмитим строку FizzBuzz
      * Если число не делится на 3,5,15 - эмитим само число
      */
-    fun task2(): Flow<String> {
-        return flowOf()
-    }
+    fun task2(): Flow<String> =
+        sampleRepository
+            .produceNumbers()
+            .transform { value ->
+                emit(value.toString())
+                when {
+                    (value % 15 == 0) -> "FizzBuzz"
+                    (value % 5 == 0) -> "Buzz"
+                    (value % 3 == 0) -> "Fizz"
+                    else -> null
+                }?.let { emit(it) }
+            }
 
     /**
      * Реализуйте функцию task3, которая объединяет эмиты из двух flow и возвращает кортеж Pair<String,String>(f1,f2),
      * где f1 айтем из первого флоу, f2 айтем из второго флоу.
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
-    fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
-    }
+    fun task3(): Flow<Pair<String, String>> =
+        sampleRepository
+            .produceColors()
+            .zip(
+                other = sampleRepository.produceForms(),
+                transform = { color, form ->
+                    color to form
+                }
+            )
 
     /**
      * Реализайте функцию task4, которая обрабатывает IllegalArgumentException и в качестве фоллбека
@@ -47,7 +67,15 @@ class SampleInteractor(
      * Если тип эксепшена != IllegalArgumentException, пробросьте его дальше
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
-    fun task4(): Flow<Int> {
-        return flowOf()
-    }
+    fun task4(): Flow<Int> =
+        sampleRepository
+            .produceNumbers()
+            .catch {
+                when (it) {
+                    is IllegalArgumentException -> emit(-1)
+                    else -> throw it
+                }
+            }.onCompletion {
+                sampleRepository.completed()
+            }
 }
