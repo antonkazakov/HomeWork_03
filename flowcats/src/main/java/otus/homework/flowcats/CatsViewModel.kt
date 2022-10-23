129332949a47728077ea691644144714ba4bc084
Loginovalexey <Loginovalexey@mail.ru>
2022-10-23 18:41:46 +0300

    Flow homework

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,31 +1,34 @@
 package otus.homework.flowcats
 
 import androidx.lifecycle.*
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsFlow = MutableStateFlow<Result?>(null)
+    val catsFlow: Flow<Result?> = _catsFlow
 
     init {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
+        viewModelScope.launch(
+            CoroutineExceptionHandler { _, throwable ->
+                _catsFlow.tryEmit(Result.Error(throwable))
+            }
+        ) {
                 catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+                    _catsFlow.value = Result.Success(it)
                 }
             }
-        }
     }
 }
 
 class CatsViewModelFactory(private val catsRepository: CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
         CatsViewModel(catsRepository) as T
 }
--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -18,7 +18,12 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .map { it * 5 }
+            .filter { it > 20 }
+            .filter { it % 2 != 0 }
+            .map { "$it won" }
+            .take(3)
     }
 
     /**
@@ -29,7 +34,24 @@ class SampleInteractor(
      * Если число не делится на 3,5,15 - эмитим само число
      */
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .transform {
+                when {
+                    it % 15 == 0 -> {
+                        emit(it.toString())
+                        emit("FizzBuzz")
+                    }
+                    it % 3 == 0 -> {
+                        emit(it.toString())
+                        emit("Fizz")
+                    }
+                    it % 5 == 0 -> {
+                        emit(it.toString())
+                        emit("Buzz")
+                    }
+                    else -> emit(it.toString())
+                }
+            }
     }
 
     /**
@@ -38,7 +60,10 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        return sampleRepository.produceColors()
+            .zip(sampleRepository.produceForms()) { first, second ->
+                Pair(first, second)
+            }
     }
 
     /**
@@ -48,6 +73,15 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch { throwable ->
+                if (throwable is IllegalArgumentException)
+                    emit(-1)
+                else
+                    throw throwable
+            }
+            .onCompletion {
+                sampleRepository.completed()
+            }
     }
 }
