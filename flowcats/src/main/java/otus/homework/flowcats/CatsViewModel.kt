691ee136882d2ccccc37f216073d43634e1620d8
Niku <niku.andrew@gmail.com>
2022-10-23 13:32:32 +0300

    Task 1-2 (Flow)

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,8 +1,9 @@
 package otus.homework.flowcats
 
 import androidx.lifecycle.*
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -10,16 +11,30 @@ class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    /*private val _catsLiveData = MutableLiveData<Fact>()
+    val catsLiveData: LiveData<Fact> = _catsLiveData*/
+    private val _uiState = MutableStateFlow<Result<Fact>>(Result.Loading("loading..."))
+    val uiState: StateFlow<Result<Fact>> = _uiState
+
+    private val handler = CoroutineExceptionHandler { _, exception ->
+        //CrashMonitor.trackWarning(exception.toString())
+        _uiState.value = Result.Error(exception.toString())
+    }
 
     init {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
-                }
+        viewModelScope.launch(handler) {
+            try {
+                catsRepository.listenForCatFacts()
+                    .flowOn(Dispatchers.IO)
+                    .collect { fact ->
+                        _uiState.value = Result.Success(fact)
+                    }
+            } catch (e: java.net.SocketTimeoutException) {
+                _uiState.value = Result.Error("Не удалось получить ответ от сервера")
             }
+            /*catch (e: retrofit2.HttpException) {
+                _uiState.value = Result.Error("404")
+            }*/
         }
     }
 }
--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -3,19 +3,48 @@ package otus.homework.flowcats
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity() {
 
     private val diContainer = DiContainer()
-    private val catsViewModel by viewModels<CatsViewModel> { CatsViewModelFactory(diContainer.repository) }
+    private val catsViewModel by viewModels<CatsViewModel> {
+        CatsViewModelFactory(diContainer.repository)
+    }
+
+    lateinit var view: CatsView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
+        view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
+        /*catsViewModel.catsLiveData.observe(this){
             view.populate(it)
+        }*/
+
+        //view.viewModel = catsViewModel
+
+        lifecycleScope.launchWhenStarted {
+            catsViewModel.uiState.collect() {
+                when (it) {
+                    is Result.Loading -> {
+                        // showProgress(...)
+                    }
+                    is Result.Success -> {
+                        updateUi(it.data)
+                    }
+                    is Result.Error -> {
+                        // showError
+                    }
+                }
+            }
         }
+    }
+
+    private fun updateUi(fact: Fact) {
+        view.populate(fact)
     }
 }
