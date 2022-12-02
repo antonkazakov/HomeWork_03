5dbf146a7215a5b6af1f778315c2052e1275cbc3
tonlirise <tonlirise@gmail.com>
2022-12-03 01:54:02 +0700

    Fix application crash in query process

--- flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
@@ -1,7 +1,10 @@
 package otus.homework.flowcats
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import retrofit2.HttpException
 
 class CatsRepository(
     private val catsService: CatsService,
@@ -10,9 +13,15 @@ class CatsRepository(
 
     fun listenForCatFacts() = flow {
         while (true) {
-            val latestNews = catsService.getCatFact()
-            emit(latestNews)
+            try {
+                val latestNews = catsService.getCatFact()
+                emit(latestNews)
+            } catch (exception: HttpException) {
+                exception.message?.let { message ->
+                    emit(Fact(message, ""))
+                }
+            }
             delay(refreshIntervalMs)
         }
-    }
+    }.flowOn(Dispatchers.IO)
 }
