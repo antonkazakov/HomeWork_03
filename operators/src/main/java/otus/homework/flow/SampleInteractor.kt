2ff02eeb517a5a9873a39af16b7a8031572d8cac
shumskii.n <lordsxrom@gmail.com>
2023-01-14 23:57:26 +0300

    Шаг 2. Поделал задачки

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -18,7 +18,12 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .map { it * 5 }
+            .filter { it > 20 }
+            .filter { it % 2 != 0 }
+            .map { "$it won" }
+            .take(3)
     }
 
     /**
@@ -29,7 +34,14 @@ class SampleInteractor(
      * Если число не делится на 3,5,15 - эмитим само число
      */
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers().transform { number ->
+            emit(number.toString())
+            when {
+                number % 15 == 0 -> emit("FizzBuzz")
+                number % 3 == 0 -> emit("Fizz")
+                number % 5 == 0 -> emit("Buzz")
+            }
+        }
     }
 
     /**
@@ -38,7 +50,8 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        return sampleRepository.produceColors()
+            .zip(sampleRepository.produceForms()) { color, form -> color to form }
     }
 
     /**
@@ -48,6 +61,14 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch { throwable ->
+                if (throwable is IllegalArgumentException) {
+                    emit(-1)
+                } else {
+                    throw throwable
+                }
+            }
+            .onCompletion { sampleRepository.completed() }
     }
 }
