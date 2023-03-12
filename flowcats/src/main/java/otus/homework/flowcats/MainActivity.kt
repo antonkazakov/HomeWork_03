602e14807d38c908d15a84484841d62b253cdd68
Negoda Timur <tnegoda@alfabank.ru>
2023-03-12 13:51:42 +0300

    Перейти с коллбеков на саспенд функции и корутины

--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -1,8 +1,11 @@
 package otus.homework.flowcats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +17,10 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
+        lifecycleScope.launch {
+            catsViewModel.catsFlow.collect {
+                view.populate(it)
+            }
         }
     }
 }
