41cfff822b626612e56d0ee679902b7e69edc95f
BagretsovDm <bagretsovdmitry@gmail.com>
2023-04-14 20:01:03 +0200

    2. Поменяйте реализацию с `LiveData` на `StateFlow` 3. В случае если возникнет ошибка в стриме, нужно заэмитить айтем `Result.Error`*
    *Создать sealed класс `Result`. Унаследовать от него классы `Success<T>`, `Error`. Использовать эти классы как стейт необходимый для рендеринга/отображени ошибки

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -2,23 +2,31 @@ package otus.homework.flowcats
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import otus.homework.flowcats.Result.*
 
 class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _state = MutableStateFlow<Result<Fact>>(Loading)
+    val state: StateFlow<Result<Fact>> get() = _state
 
     init {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.postValue(it)
-                }
+                catsRepository.listenForCatFacts()
+                    .catch { throwable ->
+                        _state.emit(Error(throwable))
+                    }
+                    .collect { fact ->
+                        _state.emit(Success(fact))
+                    }
             }
         }
     }
@@ -26,6 +34,6 @@ class CatsViewModel(
 
 class CatsViewModelFactory(private val catsRepository: CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
         CatsViewModel(catsRepository) as T
 }
--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -2,7 +2,13 @@ package otus.homework.flowcats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
+import otus.homework.flowcats.Result.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +20,24 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
+//        catsViewModel.catsLiveData.observe(this){
+//            view.populate(it)
+//        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                catsViewModel.state.collect {
+                    when (it) {
+                        is Loading -> showToast("Loading...")
+                        is Error -> it.error.message?.let { msg -> showToast(msg) }
+                        is Success -> view.populate(it.data)
+                    }
+                }
+            }
         }
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
