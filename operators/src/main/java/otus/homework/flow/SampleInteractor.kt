4be7e15d1b75b348ba8d8fdc514974835d4465b2
BagretsovDm <bagretsovdmitry@gmail.com>
2023-04-14 20:34:20 +0200

    Реализовать функции с использование flow операторов

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -18,7 +18,11 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .map { it * 5 }
+            .filter { it > 20 && (it and 1) != 0 }
+            .take(3)
+            .map { "$it won" }
     }
 
     /**
@@ -29,7 +33,19 @@ class SampleInteractor(
      * Если число не делится на 3,5,15 - эмитим само число
      */
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .transform { num ->
+                emit("$num")
+                when {
+                    num % 15 == 0 ->
+                        emit("FizzBuzz")
+                    num % 5 == 0 ->
+                        emit("Buzz")
+                    num % 3 == 0 -> {
+                        emit("Fizz")
+                    }
+                }
+            }
     }
 
     /**
@@ -38,7 +54,10 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        return sampleRepository.produceColors()
+            .zip(sampleRepository.produceForms()) { first, second ->
+                Pair(first, second)
+            }
     }
 
     /**
@@ -48,6 +67,13 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch {
+                when (it) {
+                    is IllegalArgumentException -> emit(-1)
+                    else -> throw it
+                }
+            }
+            .onCompletion { sampleRepository.completed() }
     }
 }
