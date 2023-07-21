27e0f925fa65e99228b55ccc72d5e2b224446b60
cristina <afanasyeva.kv@open.ru>
2023-07-22 02:49:12 +0300

    LiveData to StateFlow replaced. Dependencies updated

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -2,6 +2,9 @@ package otus.homework.flowcats
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -10,8 +13,12 @@ class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsLiveData = MutableStateFlow(emptyFact())
+    val catsLiveData: StateFlow<Fact> = _catsLiveData.asStateFlow()
+
+    private fun emptyFact() = Fact(
+        "", false, "", "Facts nod loaded yet", "", false, "", "", ""
+    )
 
     init {
         viewModelScope.launch {
@@ -26,6 +33,6 @@ class CatsViewModel(
 
 class CatsViewModelFactory(private val catsRepository: CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
         CatsViewModel(catsRepository) as T
 }
