96d1ac9b12f93bfc4316801de9e71fee20deda60
Alexander Kuzmin <kuzmin2706@yandex.ru>
2023-10-29 19:02:15 +0400

    hw3_part_one_pos_1_2_3

--- flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
@@ -1,12 +1,16 @@
 package otus.homework.flowcats
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 
 class CatsRepository(
     private val catsService: CatsService,
     private val refreshIntervalMs: Long = 5000
 ) {
+
+    private val scope = CoroutineScope(Dispatchers.IO)
 
     fun listenForCatFacts() = flow {
         while (true) {
@@ -15,4 +19,7 @@ class CatsRepository(
             delay(refreshIntervalMs)
         }
     }
+        .map { Result.Success(it) as Result}
+        .catch { emit(Result.Error(it)) }
+        .stateIn(scope, SharingStarted.Lazily, null)
 }
--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -1,8 +1,11 @@
 package otus.homework.flowcats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +17,18 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
+        lifecycleScope.launchWhenResumed {
+            catsViewModel.catsStateFlow.collect { result ->
+                when(result) {
+                    is Result.Success -> {
+                        result.catFact?.let { view.populate(it) }
+                    }
+                    is Result.Error -> {
+                        Log.d("MainActivity", result.throwable.message.toString())
+                    }
+                    else -> {throw RuntimeException("Wrong data received")}
+                }
+            }
         }
     }
 }
