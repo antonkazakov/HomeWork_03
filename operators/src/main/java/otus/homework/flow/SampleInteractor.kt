c76282a2b060231d6b4fa1b21b9e2faa6598613c
Boris Timofeev <mashin87@gmail.com>
2024-01-21 15:09:05 +0300

    Реализовать функции с использование flow операторов
    1. В классе `SampleInteractor` реализуйте функции `task1`-`task4` в соответствии с условиями. Для проверки функций используйте тесты в `SampleInteractorTest`

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -2,6 +2,7 @@ package otus.homework.flow
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
+import java.lang.IllegalArgumentException
 
 @ExperimentalCoroutinesApi
 class SampleInteractor(
@@ -18,7 +19,12 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .map { it * 5 }
+            .filterNot { it <= 20 }
+            .filterNot { it % 2 == 0 }
+            .map { "$it won" }
+            .take(3)
     }
 
     /**
@@ -29,7 +35,15 @@ class SampleInteractor(
      * Если число не делится на 3,5,15 - эмитим само число
      */
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .transform { num ->
+                emit(num.toString())
+                when {
+                    num % 15 == 0 -> emit("FizzBuzz")
+                    num % 3 == 0 -> emit("Fizz")
+                    num % 5 == 0 -> emit("Buzz")
+                }
+            }
     }
 
     /**
@@ -38,7 +52,10 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        return sampleRepository.produceColors()
+            .zip(sampleRepository.produceForms()) { color, form ->
+                Pair(color, form)
+            }
     }
 
     /**
@@ -48,6 +65,13 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch { err: Throwable ->
+                when (err) {
+                    is IllegalArgumentException -> emit(-1)
+                    else -> throw err
+                }
+            }
+            .onCompletion { sampleRepository.completed() }
     }
 }
