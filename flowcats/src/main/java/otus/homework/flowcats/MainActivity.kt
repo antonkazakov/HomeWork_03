1db3b48039ac4c67b04ed28e0ce27284a592b35e
victor1291 <victor449@list.ru>
2024-01-23 16:09:53 +1000

    made lifecycleScope

--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -1,18 +1,13 @@
 package otus.homework.flowcats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.CoroutineScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.buffer
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -20,7 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     private val diContainer = DiContainer()
     private val catsViewModel by viewModels<CatsViewModel> { CatsViewModelFactory(diContainer.repository) }
-    private val scope = CoroutineScope(Dispatchers.Default)
+
+    // private val scope = CoroutineScope(Dispatchers.Default)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
@@ -28,34 +24,40 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        scope.launch {
-            catsViewModel.catsData
-                .collect { state ->
-                    withContext(Dispatchers.Main) {
-                        when (state) {
-                            is Result.Success<*> -> {
-                                view.progressOff()
-                                view.populate(fact = state.valueCats as Fact)
-                            }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                catsViewModel.catsData
+                    .collect { state ->
+                        withContext(Dispatchers.Main) {
+                            when (state) {
+                                is Result.Success<*> -> {
+                                    view.progressOff()
+                                    view.populate(fact = state.valueCats as Fact)
+                                }
 
-                            is Result.Error -> {
-                                Toast.makeText(this@MainActivity," Error ${state.error}", Toast.LENGTH_LONG).show()
-                            }
-                            Result.Init -> {
-                                view.progressOn()
+                                is Result.Error -> {
+                                    Toast.makeText(
+                                        this@MainActivity,
+                                        " Error ${state.error}",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+
+                                Result.Init -> {
+                                    view.progressOn()
+                                }
                             }
                         }
                     }
-                }
+            }
         }
     }
 
-
-    override fun onStop() {
-        if (isFinishing) {
+    /*override fun onStop() {
+       *//* if (isFinishing) {
             scope.cancel("Stop PresenterScope in Activity")
-        }
+        }*//*
         super.onStop()
-    }
+    }*/
 }
 
