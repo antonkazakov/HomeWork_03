90615e2696578b7476c67d5f0e1140c294b0cd43
stanislavrepinskas <repinskas@gmail.com>
2024-01-27 15:45:16 +0300

    Done

--- flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
@@ -2,14 +2,22 @@ package otus.homework.flowcats
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
+import java.lang.RuntimeException
 
 class CatsRepository(
     private val catsService: CatsService,
     private val refreshIntervalMs: Long = 5000
 ) {
 
+    private var count = 0
+
     fun listenForCatFacts() = flow {
         while (true) {
+            count++
+            if(count % 3 == 0) {
+                throw RuntimeException("Test error")
+            }
+
             val latestNews = catsService.getCatFact()
             emit(latestNews)
             delay(refreshIntervalMs)
--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -2,7 +2,11 @@ package otus.homework.flowcats
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onErrorCollect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -10,17 +14,28 @@ class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsFlow = MutableStateFlow<Result?>(null)
+    val catsFlow = _catsFlow.asStateFlow()
 
     init {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+                catsRepository.listenForCatFacts().catch {
+                    withContext(Dispatchers.Main) {
+                        _catsFlow.value = Result.Error(it.message ?: "Unknown error")
+                    }
+                }.collect {
+                    withContext(Dispatchers.Main) {
+                        _catsFlow.value = Result.Success(it)
+                    }
                 }
             }
         }
+    }
+
+    sealed interface Result {
+        data class Success(val fact: Fact) : Result
+        data class Error(val error: String) : Result
     }
 }
 
