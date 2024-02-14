42c3d98cfca49b2b208d02c4668730d57e4d3fa7
Владислав Федюнин <v.fedunin@urent.ru>
2024-02-14 03:17:35 +0300

    homework

--- flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
@@ -1,12 +1,20 @@
 package otus.homework.flowcats
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class CatsRepository(
     private val catsService: CatsService,
     private val refreshIntervalMs: Long = 5000
 ) {
+
+    private val scope = CoroutineScope(Dispatchers.IO)
 
     fun listenForCatFacts() = flow {
         while (true) {
@@ -15,4 +23,7 @@ class CatsRepository(
             delay(refreshIntervalMs)
         }
     }
+        .map { Result.Success(it) as Result }
+        .catch { emit(Result.Error(it)) }
+        .stateIn(scope, SharingStarted.Lazily, null)
 }
--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,31 +1,19 @@
 package otus.homework.flowcats
 
-import androidx.lifecycle.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class CatsViewModel(
-    private val catsRepository: CatsRepository
+    catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
-
-    init {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
-                }
-            }
-        }
-    }
+    val catsStateFlow = catsRepository.listenForCatFacts()
 }
 
+@Suppress("UNCHECKED_CAST")
 class CatsViewModelFactory(private val catsRepository: CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
         CatsViewModel(catsRepository) as T
 }
