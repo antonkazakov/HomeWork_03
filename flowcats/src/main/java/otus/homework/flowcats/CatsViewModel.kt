5edc08bcbaaf923e13b1507e49d69e5083fbf3be
Ponyu-dev <mopoypa@yandex.ru>
2024-04-20 17:28:45 +0300

    Поменял реализацию с `LiveData` на `StateFlow`

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,6 +1,8 @@
 package otus.homework.flowcats
 
 import androidx.lifecycle.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -8,13 +10,13 @@ class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsFlow = MutableStateFlow<Fact?>(null)
+    val catsFlow = _catsFlow.asStateFlow()
 
     init {
         viewModelScope.launch {
             catsRepository.listenForCatFacts().collect {
-                _catsLiveData.value = it
+                _catsFlow.emit(it)
             }
         }
     }
