bed0af84eae9cd4527d7ff1df94ec62bbbbcefa8
vrazdyakonov <razdyakonov.viktor@gmail.com>
2024-05-02 10:27:19 +0300

    Migrate livedata to stateFlow

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,31 +1,47 @@
 package otus.homework.flowcats
 
 import androidx.lifecycle.*
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import otus.homework.flowcats.Result.Error
+import otus.homework.flowcats.Result.Success
 
 class CatsViewModel(
-    private val catsRepository: CatsRepository
+    private val catsRepository : CatsRepository,
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsStateFlow = MutableStateFlow<Result<Fact>?>(null)
+    val catsStateFlow : StateFlow<Result<Fact>?> = _catsStateFlow
 
     init {
         viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+            catsRepository
+                .listenForCatFacts()
+                .map { fact -> Success(fact) }
+                .catch { e ->
+                    if (e is CancellationException) throw e
+
+                    _catsStateFlow.tryEmit(Error(e.message ?: "Unknown error"))
                 }
-            }
+                .collect { _catsStateFlow.value = it }
         }
     }
 }
 
-class CatsViewModelFactory(private val catsRepository: CatsRepository) :
+class CatsViewModelFactory(private val catsRepository : CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass : Class<T>) : T =
         CatsViewModel(catsRepository) as T
+}
+
+sealed class Result<T> {
+    data class Success<T>(val data : T) : Result<T>()
+    data class Error<T>(val message : String) : Result<T>()
 }
--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -2,20 +2,43 @@ package otus.homework.flowcats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import otus.homework.flowcats.Result.Error
+import otus.homework.flowcats.Result.Success
 
 class MainActivity : AppCompatActivity() {
 
     private val diContainer = DiContainer()
     private val catsViewModel by viewModels<CatsViewModel> { CatsViewModelFactory(diContainer.repository) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                catsViewModel.catsStateFlow.collect { result : Result<Fact>? ->
+                    result ?: return@collect
+
+                    when (result) {
+                        is Success -> view.populate(result.data)
+                        is Error -> Toast
+                            .makeText(
+                                this@MainActivity.baseContext,
+                                result.message,
+                                Toast.LENGTH_SHORT
+                            )
+                            .show()
+                    }
+                }
+            }
         }
     }
 }
