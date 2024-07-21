98c366c528a1d7927199a4399444e4ade10fbe8a
bmv-2143 <bmv-2143@yandex.ru>
2024-07-21 12:53:49 +0300

    feat: implement tasks 1.2 - 1.3

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,8 +1,11 @@
 package otus.homework.flowcats
 
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -10,15 +13,16 @@ class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsStateFlow = MutableStateFlow<Result<Fact>?>(null)
+    val catsStateFlow = _catsStateFlow.asStateFlow()
 
     init {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
-                }
+                catsRepository.listenForCatFacts()
+                    .collect {
+                        _catsStateFlow.value = it
+                    }
             }
         }
     }
--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -1,11 +1,18 @@
 package otus.homework.flowcats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private val tag: String = javaClass.simpleName
     private val diContainer = DiContainer()
     private val catsViewModel by viewModels<CatsViewModel> { CatsViewModelFactory(diContainer.repository) }
 
@@ -14,8 +21,28 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
+        observeCatsFlow(view)
+    }
+
+    private fun observeCatsFlow(view: CatsView) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                catsViewModel.catsStateFlow.collect {
+                    when (it) {
+                        is Result.Success -> view.populate(it.data)
+                        is Result.Error -> showError(it.error)
+                        else -> Log.e(tag, ERROR_UNKNOWN_STATE)
+                    }
+                }
+            }
         }
+    }
+
+    private fun showError(error: Throwable) {
+        Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val ERROR_UNKNOWN_STATE = "Unknown state"
     }
 }
