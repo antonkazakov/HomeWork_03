26e3f55d56560d8c02b76390e8051903c6c5e8be
bmv-2143 <bmv-2143@yandex.ru>
2024-07-21 14:31:53 +0300

    feat: implement tasks 2.1: task4()

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -2,9 +2,10 @@ package otus.homework.flow
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNot
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.flow.zip
@@ -80,6 +81,14 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch {
+                if (it is IllegalArgumentException) {
+                    emit(-1)
+                } else {
+                    throw it
+                }
+            }
+            .onCompletion { sampleRepository.completed() }
     }
 }
