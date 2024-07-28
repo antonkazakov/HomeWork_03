e24a85e1d051abb9f2d53b4ad1b1442220b31f37
bmv-2143 <bmv-2143@yandex.ru>
2024-07-28 16:58:35 +0300

    refactor(pr183): clean up SampleInteractor.task2()

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -43,22 +43,11 @@ class SampleInteractor(
     fun task2(): Flow<String> {
         return sampleRepository.produceNumbers()
             .transform {
+                emit("$it")
                 when {
-                    it % 15 == 0 -> {
-                        emit("$it")
-                        emit("FizzBuzz")
-                    }
-                    it % 3 == 0 -> {
-                        emit("$it")
-                        emit("Fizz")
-                    }
-                    it % 5 == 0 -> {
-                        emit("$it")
-                        emit("Buzz")
-                    }
-                    else -> {
-                        emit("$it")
-                    }
+                    it % 15 == 0 -> emit("FizzBuzz")
+                    it % 3 == 0 -> emit("Fizz")
+                    it % 5 == 0 -> emit("Buzz")
                 }
             }
     }
