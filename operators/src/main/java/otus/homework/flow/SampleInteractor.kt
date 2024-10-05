0b9cb8adf266effee76796e62ba2002a0d0f417b
Tatyana Ivanova <tatyana.skorbilina@yandex.ru>
2024-10-05 15:05:54 +0300

    Реализованы функции с использование flow операторов

--- operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -18,7 +18,11 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers().map{value -> value*5}
+        .filter { value -> value > 20 }
+            .filter {value -> (value % 2) != 0}
+            .map {value -> "$value won"}
+            .take(3)
     }
 
     /**
@@ -29,7 +33,25 @@ class SampleInteractor(
      * Если число не делится на 3,5,15 - эмитим само число
      */
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .flatMapConcat {  value ->
+                flow{
+                    if (value % 15 == 0) {
+                        emit("$value")
+                        emit("FizzBuzz")
+                    }
+                    else if (value % 3 == 0){
+                        emit("$value")
+                        emit("Fizz")
+                    }
+                    else if  (value % 5 == 0){
+                        emit("$value")
+                        emit("Buzz")
+                    }
+                    else
+                        emit ("$value")
+                }
+            }
     }
 
     /**
@@ -38,7 +60,9 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        return sampleRepository.produceColors().zip(sampleRepository.produceForms()) {
+            f1,f2 -> Pair(f1,f2)
+        }
     }
 
     /**
@@ -48,6 +72,9 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers().catch { e ->
+            if (e is IllegalArgumentException) emit( -1)
+            else throw e}
+            .onCompletion { sampleRepository.completed() }
     }
 }
