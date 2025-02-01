fdf215626a09f1f94aa25897aa71d018d3282bb5
Beknur Patsaev <patsaev1994@mail.ru>
2025-02-02 00:45:29 +0300

    1. Сейчас приложение крашится. Поправьте ошибку. - перестал сетить в LiveData на IO диспатчере

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -3,6 +3,8 @@ package otus.homework.flowcats
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.produceIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -15,17 +17,17 @@ class CatsViewModel(
 
     init {
         viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
+            catsRepository.listenForCatFacts()
+                .flowOn(Dispatchers.IO)
+                .collect {
                     _catsLiveData.value = it
                 }
-            }
         }
     }
 }
 
 class CatsViewModelFactory(private val catsRepository: CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
         CatsViewModel(catsRepository) as T
 }
