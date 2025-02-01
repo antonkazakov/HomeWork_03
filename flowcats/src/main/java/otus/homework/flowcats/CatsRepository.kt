a1352b8871eee9c5ea2cacce6770c9974f584263
adil.ziganshin <adilziga@gmail.com>
2025-02-01 13:02:00 +0300

    hw

--- flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsRepository.kt
@@ -3,6 +3,11 @@ package otus.homework.flowcats
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 
+sealed class Result {
+    class Success(val fact: Fact): Result()
+    class Error(val exception: Throwable): Result()
+}
+
 class CatsRepository(
     private val catsService: CatsService,
     private val refreshIntervalMs: Long = 5000
@@ -10,8 +15,12 @@ class CatsRepository(
 
     fun listenForCatFacts() = flow {
         while (true) {
-            val latestNews = catsService.getCatFact()
-            emit(latestNews)
+            try {
+                val latestFact = catsService.getCatFact()
+                emit(Result.Success(latestFact))
+            } catch (exception: Throwable) {
+                emit(Result.Error(exception))
+            }
             delay(refreshIntervalMs)
         }
     }
